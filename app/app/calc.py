d2244a05a4e09f30a9d9f5097b834e5fc5b875f1
Sabu Varghese <sabutvarghese@gmail.com>
2023-05-13 23:30:22 -0400

    installing core and adding core to installed apps

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y and retun result """
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and retun result """
+    return y - x
